1ccef32558087bed6fcaa70aa9d24188984d2a22
Muralisanku <sankumurali4a6@gmail.com>
2025-07-16 08:24:30 +0530

    jenkins

---
+++ 04-databases/variables.tf
@@ -0,0 +1,19 @@
+variable "common_tags" {
+  default = {
+    Project = "roboshop"
+    Environment = "dev"
+    Terrafrom = "true"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "joinaiops.store"
+}
