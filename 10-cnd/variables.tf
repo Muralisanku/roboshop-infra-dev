cbda47f3841cde8483f54850eba2752ee889bad7
Muralisanku <sankumurali4a6@gmail.com>
2025-07-16 08:25:43 +0530

    jenkins

---
+++ 10-cnd/variables.tf
@@ -0,0 +1,25 @@
+variable "common_tags" {
+  default = {
+    Project = "roboshop"
+    Environment = "dev"
+    Terrafrom = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "cdn"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "joinaiops.store"
+}
